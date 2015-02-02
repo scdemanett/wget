fbdec1b4e3978e01e7b5eb1f8018c992871ce933
Ricardo Padilha <ricardospadilha@gmail.com>
2015-02-02 14:19:56 +0100

    First version

---
+++ app.sh
@@ -0,0 +1,70 @@
+### ZLIB ###
+_build_zlib() {
+local VERSION="1.2.8"
+local FOLDER="zlib-${VERSION}"
+local FILE="${FOLDER}.tar.gz"
+local URL="http://zlib.net/${FILE}"
+
+_download_tgz "${FILE}" "${URL}" "${FOLDER}"
+pushd "target/${FOLDER}"
+./configure --prefix="${DEPS}" --libdir="${DEST}/lib"
+make
+make install
+rm -v "${DEST}/lib/libz.a"
+popd
+}
+
+### OPENSSL ###
+_build_openssl() {
+local OPENSSL_VERSION="1.0.2"
+local OPENSSL_FOLDER="openssl-${OPENSSL_VERSION}"
+local OPENSSL_FILE="${OPENSSL_FOLDER}.tar.gz"
+local OPENSSL_URL="http://www.openssl.org/source/${OPENSSL_FILE}"
+
+_download_tgz "${OPENSSL_FILE}" "${OPENSSL_URL}" "${OPENSSL_FOLDER}"
+pushd "target/${OPENSSL_FOLDER}"
+./Configure --prefix="${DEPS}" \
+  --openssldir="${DEST}/etc/ssl" \
+  --with-zlib-include="${DEPS}/include" \
+  --with-zlib-lib="${DEST}/lib" \
+  shared zlib-dynamic threads linux-armv4 -DL_ENDIAN ${CFLAGS} ${LDFLAGS}
+sed -i -e "s/-O3//g" Makefile
+make -j1
+make install_sw
+cp -avR "${DEPS}/lib"/* "${DEST}/lib/"
+rm -vfr "${DEPS}/lib"
+rm -vf "${DEST}/lib/libcrypto.a" "${DEST}/lib/libssl.a"
+sed -i -e "s|^exec_prefix=.*|exec_prefix=${DEST}|g" "${DEST}/lib/pkgconfig/openssl.pc"
+popd
+}
+
+### WGET ###
+_build_wget() {
+local VERSION="1.16.1"
+local FOLDER="wget-${VERSION}"
+local FILE="${FOLDER}.tar.xz"
+local URL="http://ftp.gnu.org/gnu/wget/${FILE}"
+
+_download_xz "${FILE}" "${URL}" "${FOLDER}"
+pushd "target/${FOLDER}"
+./configure --host="${HOST}" --prefix="${DEST}" --mandir="${DEST}/man"  --with-ssl=openssl --disable-pcre
+make
+make install
+echo "ca_certificate = ${DEST}/etc/ssl/certs/ca-certificates.crt" >> "${DEST}/etc/wgetrc"
+popd
+}
+
+### CERTIFICATES ###
+_build_certificates() {
+# update CA certificates on a Debian/Ubuntu machine:
+#sudo update-ca-certificates
+cp -vf /etc/ssl/certs/ca-certificates.crt "${DEST}/etc/ssl/certs/"
+}
+
+_build() {
+  _build_zlib
+  _build_openssl
+  _build_wget
+  _build_certificates
+  _package
+}
