c8a97ff955ff35724e0ea6bce3f9db6c80efdef0
Ricardo Padilha <ricardospadilha@gmail.com>
2015-06-08 15:06:40 -0700

    Replacement for embedded busybox wget

--- app.sh
+++ app.sh
@@ -1,40 +1,37 @@
+LDFLAGS="-L${DEPS}/lib"
+
 ### ZLIB ###
 _build_zlib() {
-local VERSION="1.2.8"
+# Closest to the one that ships with the 5N (v1.2.3.4)
+local VERSION="1.2.3"
 local FOLDER="zlib-${VERSION}"
 local FILE="${FOLDER}.tar.gz"
-local URL="http://zlib.net/${FILE}"
+local URL="http://downloads.sourceforge.net/project/libpng/zlib/${VERSION}/${FILE}"
 
 _download_tgz "${FILE}" "${URL}" "${FOLDER}"
 pushd "target/${FOLDER}"
-./configure --prefix="${DEPS}" --libdir="${DEST}/lib"
+./configure --prefix="${DEPS}" --shared
 make
 make install
-rm -v "${DEST}/lib/libz.a"
 popd
 }
 
 ### OPENSSL ###
 _build_openssl() {
-local VERSION="1.0.2a"
+# Same as the one that ships with the 5N
+local VERSION="0.9.8n"
 local FOLDER="openssl-${VERSION}"
 local FILE="${FOLDER}.tar.gz"
-local URL="http://www.openssl.org/source/${FILE}"
+local URL="https://www.openssl.org/source/old/0.9.x/${FILE}"
 
 _download_tgz "${FILE}" "${URL}" "${FOLDER}"
-cp -vf "src/${FOLDER}-parallel-build.patch" "target/${FOLDER}/"
 pushd "target/${FOLDER}"
-patch -p1 < "${FOLDER}-parallel-build.patch"
-./Configure --prefix="${DEPS}" shared threads linux-armv4 --openssldir="${DEST}/etc/ssl" \
-  zlib-dynamic --with-zlib-include="${DEPS}/include"--with-zlib-lib="${DEST}/lib" \
+./Configure --prefix="${DEPS}" shared threads linux-generic32 \
+  zlib-dynamic --with-zlib-include="${DEPS}/include" --with-zlib-lib="${DEPS}/lib" \
   -DL_ENDIAN ${CFLAGS} ${LDFLAGS}
-sed -i -e "s/-O3//g" Makefile
+sed -e "s/-O3//g" -i Makefile
 make -j1
 make install_sw
-cp -avR "${DEPS}/lib"/* "${DEST}/lib/"
-rm -vfr "${DEPS}/lib"
-rm -vf "${DEST}/lib/libcrypto.a" "${DEST}/lib/libssl.a"
-sed -i -e "s|^exec_prefix=.*|exec_prefix=${DEST}|g" "${DEST}/lib/pkgconfig/openssl.pc"
 popd
 }
 
@@ -47,20 +44,18 @@ local URL="http://ftp.gnu.org/gnu/wget/${FILE}"
 
 _download_xz "${FILE}" "${URL}" "${FOLDER}"
 pushd "target/${FOLDER}"
-PKG_CONFIG_PATH="${DEST}/lib/pkgconfig" ./configure --host="${HOST}" --prefix="${DEST}" --mandir="${DEST}/man"  --with-ssl=openssl --with-openssl=yes --with-libssl-prefix="${DEST}" --disable-pcre
+./configure --host="${HOST}" --prefix="" --localedir="/usr/share/locale" \
+  --with-ssl=openssl --with-openssl=yes --with-libssl-prefix="${DEPS}" --disable-pcre --disable-rpath
 make
-make install
-echo "ca_certificate = ${DEST}/etc/ssl/certs/ca-certificates.crt" >> "${DEST}/etc/wgetrc"
-mv -f "${DEST}/etc/wgetrc" "${DEST}/etc/wgetrc.default"
+make install DESTDIR="${DEST}"
+"${STRIP}" -s -R .comment -R .note -R .note.ABI-tag "${DEST}/bin/wget"
+echo "ca_certificate = /etc/cacert.pem" >> "${DEST}/etc/wgetrc"
 popd
 }
 
 ### CERTIFICATES ###
 _build_certificates() {
-# update CA certificates on a Debian/Ubuntu machine:
-#sudo update-ca-certificates
-#cp -vf /etc/ssl/certs/ca-certificates.crt "${DEST}/etc/ssl/certs/"
-wget -O "${DEST}/etc/ssl/certs/ca-certificates.crt" "http://curl.haxx.se/ca/cacert.pem"
+wget -O "${DEST}/etc/cacert.pem" "http://curl.haxx.se/ca/cacert.pem"
 }
 
 _build() {
